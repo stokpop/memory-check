ba66fc817ca0ecdf369044164115e36aa2b5fcb5
Peter Paul Bakker <peter.paul.bakker@stokpop.nl>
2025-09-15 22:35:54 +0200

    update gradle and all dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,29 +7,30 @@ version = file("VERSION").readText().trim()
 description = "memory-check"
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.4.32"
-    kotlin("plugin.serialization") version "1.4.32"
-    id("org.jetbrains.dokka") version "1.4.30"
-    id("com.github.hierynomus.license") version "0.15.0"
-    id("com.vanniktech.maven.publish") version "0.14.2"
+    kotlin("jvm") version "2.2.20"
+    kotlin("plugin.serialization") version "2.2.20"
+    id("org.jetbrains.dokka") version "2.0.0"
+    id("com.github.hierynomus.license") version "0.16.1"
+    id("com.vanniktech.maven.publish") version "0.34.0"
     // check dependency updates: ./gradlew dependencyUpdates -Drevision=release
-    id("com.github.ben-manes.versions") version "0.38.0"
+    id("com.github.ben-manes.versions") version "0.52.0"
+    id("org.sonarqube") version "6.3.1.5724"
     application
 }
 
 repositories {
-    jcenter()
+    mavenCentral()
 }
 
 dependencies {
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
-    implementation("com.github.ajalt.clikt:clikt:3.1.0")
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:2.2.20"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+    implementation("com.github.ajalt.clikt:clikt:5.0.3")
+    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.9.25")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.13.4")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.13.4")
 }
 
 tasks.test {
@@ -70,7 +71,7 @@ val fatJar = task("fatJar", type = Jar::class) {
 // prevent alpha releases as a suggestion to update in dependencyUpdates
 // see: https://github.com/ben-manes/gradle-versions-plugin#revisions
 fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
     val regex = "^[0-9,.v-]+(-r)?$".toRegex()
     val isStable = stableKeyword || regex.matches(version)
     return isStable.not()
